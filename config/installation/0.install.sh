4698ee6ad3aaab0cb08fc6ea61b165012ff59149
Alise Ponsero <alise.ponsero@gmail.com>
2023-11-15 12:52:30 +0200

    Installation QC tools in Tykky containers

--- config/installation/0.install.sh
+++ config/installation/0.install.sh
@@ -11,6 +11,17 @@ conda-containerize new --prefix Kraken_Braken kraken_braken.yml
 ## HumGut database download
 # direct download from https://arken.nmbu.no/~larssn/humgut/
 
+### Bowtie2 and trim-galore installation
+# creating tykky container for QC tools
+
+module purge
+module load tykky
+mkdir QC_tools
+conda-containerize new --prefix QC_tools QC_env.yml
+
+## Human genome download
+# Download the bowtie2 database for the human genome from https://genome-idx.s3.amazonaws.com/bt/GRCh38_noalt_as.zip
+
 ### Humann3 installation
 # Humann3 and Metaphlan are available on Puhti modules
 # https://docs.csc.fi/apps/metaphlan/
